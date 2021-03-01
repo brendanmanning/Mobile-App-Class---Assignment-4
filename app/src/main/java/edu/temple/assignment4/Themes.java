d930fb90f61f40943e3bfe129a54b5787a21edbf
Brendan Manning <>
2021-02-28 21:16:24 -0500

    Move themes to a separate data class

---
+++ app/src/main/java/edu/temple/assignment4/Themes.java
@@ -0,0 +1,9 @@
+package edu.temple.assignment4;
+
+public class Themes {
+    static final Theme[] themes = new Theme[] {
+         new Theme("Dog", R.drawable.dog),
+         new Theme("Cat", R.drawable.cat),
+         new Theme("Acadia", R.drawable.acadia)
+    };
+}
