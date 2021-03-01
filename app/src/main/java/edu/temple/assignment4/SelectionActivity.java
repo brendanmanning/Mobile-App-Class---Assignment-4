0ea4586da041701b2f5784ea0b79f1c14ad8491c
Brendan Manning <>
2021-02-28 21:16:32 -0500

    Update SelectionActivity.java

--- app/src/main/java/edu/temple/assignment4/SelectionActivity.java
+++ app/src/main/java/edu/temple/assignment4/SelectionActivity.java
@@ -2,7 +2,11 @@ package edu.temple.assignment4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class SelectionActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Item Selection");
+
+        Spinner spinner = findViewById(R.id.spinnerView);
+        spinner.setAdapter(new ImageAdapter(this, Themes.themes));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) return;
+                Intent launchIntent = new Intent(SelectionActivity.this, DisplayActivity.class);
+                launchIntent.putExtra("theme", position-1);
+                startActivity(launchIntent);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
