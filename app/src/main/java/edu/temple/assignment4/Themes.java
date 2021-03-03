2a6ea9c39d33665c7962a573914a3c95f91d9e11
Brendan Manning <>
2021-03-03 10:36:53 -0500

    Update image resources
    (relates to the theme change commit I previously made)

--- app/src/main/java/edu/temple/assignment4/Themes.java
+++ app/src/main/java/edu/temple/assignment4/Themes.java
@@ -2,8 +2,8 @@ package edu.temple.assignment4;
 
 public class Themes {
     static final Theme[] themes = new Theme[] {
-         new Theme("Dog", R.drawable.dog),
-         new Theme("Cat", R.drawable.cat),
-         new Theme("Acadia", R.drawable.acadia)
+         new Theme("Acadia", R.drawable.acadia),
+         new Theme("Congaree", R.drawable.congaree),
+         new Theme("Great Smoky Mountain", R.drawable.smokies)
     };
 }
