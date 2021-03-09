b1812a2dc6f6f16cb157393a90108af94e6ec9ed
Brendan Manning <>
2021-03-09 09:01:48 -0500

    More themes

--- app/src/main/java/edu/temple/assignment5/Themes.java
+++ app/src/main/java/edu/temple/assignment5/Themes.java
@@ -4,6 +4,10 @@ public class Themes {
     static final Theme[] themes = new Theme[] {
          new Theme("Acadia", R.drawable.acadia),
          new Theme("Congaree", R.drawable.congaree),
-         new Theme("Great Smoky Mountain", R.drawable.smokies)
+         new Theme("Great Smoky Mountain", R.drawable.smokies),
+         new Theme("Shenandoah", R.drawable.shenandoah),
+         new Theme("Everglades", R.drawable.everglades),
+         new Theme("Arches", R.drawable.arches),
+         new Theme("Badlands", R.drawable.badlands)
     };
 }
