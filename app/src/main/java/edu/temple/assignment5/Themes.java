bf92995c45358a1ce436a3a8adac96a4d8b6ff9b
Brendan Manning <>
2021-03-15 20:56:57 -0400

    Add theme names from string file

--- app/src/main/java/edu/temple/assignment5/Themes.java
+++ app/src/main/java/edu/temple/assignment5/Themes.java
@@ -1,13 +1,28 @@
 package edu.temple.assignment5;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 public class Themes {
-    static final Theme[] themes = new Theme[] {
-         new Theme("Acadia", R.drawable.acadia),
-         new Theme("Congaree", R.drawable.congaree),
-         new Theme("Great Smoky Mountain", R.drawable.smokies),
-         new Theme("Shenandoah", R.drawable.shenandoah),
-         new Theme("Everglades", R.drawable.everglades),
-         new Theme("Arches", R.drawable.arches),
-         new Theme("Badlands", R.drawable.badlands)
+
+    private static Theme[] themes = new Theme[0];
+
+    static void setThemes(Resources resources) {
+        String[] park_names = resources.getStringArray(R.array.selection_activity_item_names);
+        themes = new Theme[] {
+                new Theme(park_names[0], R.drawable.acadia),
+                new Theme(park_names[1], R.drawable.congaree),
+                new Theme(park_names[2], R.drawable.smokies),
+                new Theme(park_names[3], R.drawable.shenandoah),
+                new Theme(park_names[4], R.drawable.everglades),
+                new Theme(park_names[5], R.drawable.arches),
+                new Theme(park_names[6], R.drawable.badlands)
+        };
+
+    }
+
+
+    static Theme[] getThemes() {
+        return themes;
     };
 }
